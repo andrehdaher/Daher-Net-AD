4c5a12139ac72f769e7f24f1caab83cf43d13f5d
andrehdaher <andrehdaher2003@gmail.com>
2025-02-15 09:48:46 +0300

    تم التعديل في index.js وتعديل في مجلد front

--- backend/index.js
+++ backend/index.js
@@ -532,7 +532,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../front/hifi/build", "index.html"));
 });
 
-const PORT = process.env.PORT || 3000; // Render يوفر المنفذ عبر متغير البيئة PORT
+const PORT = process.env.PORT || 10000; // Render يوفر المنفذ عبر متغير البيئة PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
